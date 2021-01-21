73549661fe4e8277cc9302e799778357b42b5cd8
Sebastian Martinez <bargainbend@gmail.com>
2021-01-21 18:26:17 +0800

    Changed App.js setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
-import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
-=======
-import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
->>>>>>> 9bf0d350829d56e75ba0e7a9b54a9d25de17781d
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
 function App() {
 	return (
 		<div className='container'>
